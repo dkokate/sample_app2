2df724bdbf2f4b6b173f89e74b2a338665acac39
dkokate@gmail.com <dkokate@gmail.com>
2013-07-23 14:28:38 -0400

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Sign the user in and redirect to the user's show pagesign_in user
+      # Sign the user in and redirect to the user's show page sign_in user
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       # Create an error message and re-render the sign in page
       flash.now[:error] = 'Invalid email/password combination' 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  
+  def index
+    @users = User.paginate(page: params[:page], per_page: 30).order('name COLLATE NOCASE ASC')
+    # COLLATE NOCASE ASC orders the result irrespective of the whether the name is in uppercase or lowercase
+  end
+  
   def new
     @user = User.new
   end
@@ -18,9 +27,48 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    # @user = User.find(params[:id])  removed because becfore_action correct_user gets triggered 
+  end
+  
+  def update
+    # @user = User.find(params[:id]) removed because becfore_action correct_user gets triggered 
+    if @user.update_attributes(user_params)
+      #Handle a successful update
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_url
+  end
+  
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
+  # Before filters
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in" 
+    end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user) 
+    # Note: current_user? method has been defined in SessionsHelper module
+  end
+  
+  def admin_user
+    redirect_to(rooth_path) unless current_user.admin?
+  end
 end
