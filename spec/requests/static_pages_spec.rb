5c06412d479b0e2e9bb1a18e3977358d36da7eb9
dkokate@gmail.com <dkokate@gmail.com>
2013-08-01 13:56:59 -0400

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,7 +11,7 @@ describe "Static pages" do
     it { should have_title(full_title('')) }
     it { should_not have_title('Home') }
     
-    describe "Home page" do
+    describe "for signed-in users" do
       let(:user) {FactoryGirl.create(:user)}
       before do
         FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
@@ -25,6 +25,16 @@ describe "Static pages" do
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
       end
+      
+      describe" following/follower counts" do
+      end
+      let(:other_user) {FactoryGirl.create(:user)}
+      before do
+        other_user.follow!(user)
+        visit root_path
+      end
+      it {should have_link("0 following", href: following_user_path(user))}
+      it {should have_link("1 followers", href: followers_user_path(user))}
     end
   end
   
